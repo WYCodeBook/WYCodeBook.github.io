f42fcf0c33cac22834cf5dbadbf97a6fa5378c2b
WYCodeBook <wyu10amy@hotmail.com>
2019-06-03 14:48:41 -0700

    try

--- js/button.js
+++ js/button.js
@@ -1,8 +1,8 @@
-// When the user scrolls down 100px from the top of the document, show the button
+// When the user scrolls down 1000px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
