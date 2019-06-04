c61f8c0ef8f62c5b290ac7a340768ba92b958350
WYCodeBook <wyu10amy@hotmail.com>
2019-06-03 20:28:01 -0700

    try

--- js/button.js
+++ js/button.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
