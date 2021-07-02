6263bde0feae098b569965245f6fd6c81f6bb750
WY <wyu10amy@hotmail.com>
2021-07-01 20:39:25 -0700

    added repos section utilized the GitHub API and JavaScript

---
+++ _site/js/repoAPI.js
@@ -0,0 +1,54 @@
+
+//Listing all the repositories
+
+function requestUserRepos(username){
+
+    // Create new XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+
+    // GitHub endpoint, dynamically passing in specified username
+    const url = `https://api.github.com/users/${username}/repos?per_page=20`;
+
+    // Open a new connection, using a GET request via URL endpoint
+    // Providing 3 arguments (GET/POST, The URL, Async True/False)
+    xhr.open('GET', url, true);
+
+    // When request is received
+    // Process it here
+    xhr.onload = function () {
+
+        // Parse API data into JSON
+        const data = JSON.parse(this.response);
+      
+      
+        // Loop over each object in data array
+        for (let i in data) {
+
+            // Get the ul with id of of userRepos
+            let ul = document.getElementById('userRepos');
+
+            // Create variable that will create li's to be added to ul
+            let li = document.createElement('li');
+
+            // Add Bootstrap list item class to each li
+            li.classList.add('list-group-item')
+
+            // Create the html markup for each li
+            li.innerHTML = (`
+                <p><a href="${data[i].html_url}" target="_blank"><div><strong>${data[i].name} </strong> (${data[i].language})</a></p>
+            `);
+         
+          
+
+            // Append each li to the ul
+            ul.appendChild(li);
+
+        }
+
+    }
+
+    // Send the request to the server
+    xhr.send();
+
+}
+requestUserRepos('Curious-Yu');
